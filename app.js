63d6af9823b17fdd9e9a8e296abd194debef4616
hao812812 <howard881122@gmail.com>
2023-06-26 21:59:08 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars');
+
+
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
-
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -22,12 +24,19 @@ db.once('open', () => {
 })
 
 
-// 設定首頁路由
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
-// 設定 port 3000
+
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
+
+
+
+
+// 設定首頁路由
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
